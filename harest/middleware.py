1f048de546a57f02e7e4e839f2a8b880434c53e1
jpardobl <kit2707888>
2014-08-28 17:21:34 +0200

    made mor verbose when errors

--- harest/middleware.py
+++ harest/middleware.py
@@ -12,13 +12,14 @@ class Exception2Json():
     Middleware meant to process exception found at views
     """
     def process_exception(self, request, exception):
-        logger.error(exception)
+        logger.error("EXCEPTION: %s" % exception)
         if isinstance(exception, Http404):
             return HttpResponseNotFound(
                 content=simplejson.dumps({"errors": [str(exception)]}),
                 content_type="application/json",)
 
         logger.error(traceback.format_exc())
+        msg = "Server error: %s" % exception if settings.DEBUG else "Server error"
         return HttpResponseServerError(
-            content=simplejson.dumps({"errors": ["Server error", ]}),
+            content=simplejson.dumps({"errors": [msg, ]}),
             content_type="application/json",)
